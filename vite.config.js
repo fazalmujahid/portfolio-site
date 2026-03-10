633174d8b5f449f748f9f085078f0fb328e71a2c
Imaduddin Mujahid <imaduddin.mujahid@nagarro.com>
2026-03-10 17:28:42 +0530

    url changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio-site/",
+  base: "/",
   plugins: [react()],
   server: {
     proxy: {
